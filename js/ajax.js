d83c468bed779c18a3b651c3bafad8adc8d3014b
iFHA <fernando.everywhere@gmail.com>
2017-11-30 00:24:46 -0300

    ajax adicionado

---
+++ js/ajax.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#buscar').click(function () {
+        buscar($("#palavra").val())
+    });
+    var dadozin = $("#dados");//document.getElementById("dados");
+    function carregando(c){
+        removeFilhos(c);
+        var img = $("<img/>", {
+            src: "./ajax-loader.gif"
+        });
+        c.append(img);
+    }
+    function removeFilhos(c){
+        c.empty();
+    }
+    function buscar(palavra)
+    {
+        var page = "busca.php";
+        $.ajax({
+            type: 'POST',
+            dataType: 'html',
+            url: page,
+            beforeSend: function () {
+                //$("#dados").html("Carregando...");
+                carregando(dadozin);
+            },
+            data: {palavra: palavra},
+            success: function (msg)
+            {
+                removeFilhos(dadozin);
+                $("#dados").html(msg);
+            }
+        });
+    }
+});
